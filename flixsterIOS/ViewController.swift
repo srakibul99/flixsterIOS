984a64830a087b170103ee20929bc3d78a004ff8
Shakira Rakibullah <srakibul@ucsd.edu>
2020-10-18 16:50:40 -0700

    Initial Commit

---
+++ flixsterIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixsterIOS
+//
+//  Created by Shakira Rakibullah on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
