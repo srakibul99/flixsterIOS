42706c47a2039d41ccdf22e5be1d2c573f868b7b
Shakira Rakibullah <srakibul@ucsd.edu>
2020-10-19 13:05:16 -0700

    implemented required user stories

---
+++ flixsterIOS/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixsterIOS
+//
+//  Created by Shakira Rakibullah on 10/19/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
